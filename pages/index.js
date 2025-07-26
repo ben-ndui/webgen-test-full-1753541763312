75b8ac26b8b634049efabffa8f3c5e6f56fd38bf
Ben NDUI <ben.ndui@gmail.com>
2025-07-26 16:56:07 +0200

    Add homepage

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'system-ui' }}>
+      <h1>🎉 Site généré par WebGen AI</h1>
+      <p>Ce site a été créé automatiquement et déployé !</p>
+      <p>Repository: <a href="https://github.com/ben-ndui/webgen-test-full-1753541763312" target="_blank">https://github.com/ben-ndui/webgen-test-full-1753541763312</a></p>
+    </div>
+  )
+}
